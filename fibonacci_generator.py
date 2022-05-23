6cdbb5fe1ed661976177638283ee7335f40956c0
xColdNox <102612330+xColdNox@users.noreply.github.com>
2022-05-23 23:28:04 +0200

    Update fibonacci_generator.py

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,69 +1,65 @@
-from time import *
+import time
 
-# I tried to create Fibonacci sequence that asks for input repeatedly.
-# First we ask for a number to calculate Fibonacci sequence and make sure it's a number.
 
-while True:
-    user_number1 = input("How long do you want the Fibonacci sequence to be? Enter a number: ")
-    try:
-        n1 = int(user_number1)
-        break
-    except ValueError:
-        print("It has to be a number!")
+def get_number():
+    """
+    Function that asks the user for an integer and validates that it is a positive number.
+    :return: validated user input
+    :rtype: int
+    """
+    while True:
+        user_number = input("How long do you want the Fibonacci sequence to be? Enter a number: ")
+        try:
+            n = int(user_number)
 
-# Further, we check if it's not a negative value.
+            # Further, we check if it's not a negative value.
+            if n < 1:
+                print("Number must be positive.\n")
+            else:
+                break
+        except ValueError:
+            print("It has to be a number!\n")
+    return n
 
-while n1 < 0:
-    print("Invalid number!\n")
-    user_number1 = input("Try again, positive value this time: ")
-    n1 = int(user_number1)
-print("You got it! Here comes the Fibonacci sequence:")
-    
-# Then we define the Fibonacci sequence.
 
 def fibonacci(n):
-    a = 0
-    b = 1
-    if n < 0:
-        print("Please enter a positive number!\n")
-    elif n == 0:
-        return a
-    elif n == 1 or n == 2:
-        return b
-    else:
-        for i in range(1,n):
-            c = a + b
-            a = b
-            b = c
-        return c
+    """
+    Generates and returns a fibonacci sequence.
+    :param n: positive integer defining sequence length
+    :return: fibonacci sequence list
+    :rtype: list
+    """
+    # Then we define the Fibonacci sequence.
+    sequence = [0, 1]
+    if n < 3:
+        return sequence[:n]
+    for i in range(n-2):
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
 
-# We print out the result.
 
-for n in range(n1):
+def ask_for_more():
+    """
+    Asks user if wants to continue.
+    :return: True for "yes" and False for "no"
+    :rtype: bool
+    """
+    while True:
+        answer = input("There you go. Do you want to try another number? Enter (yes/no): ")
+        if answer not in ["yes", "no"]:
+            print("Invalid answer, please enter 'yes' or 'no'.\n")
+        elif answer == "no":
+            print("Ok, glad I could help!")
+            time.sleep(4)
+            return False
+        elif answer == "yes":
+            return True
+
+
+if __name__ == "__main__":
+    n = get_number()
     print(fibonacci(n))
 
-# We proceed to the step where it will continously ask us if we want to calculate another Fibonacci sequence.
-
-while True:
-    user_number2 = input("There you go. Do you want to try another number? Enter (yes/no): ")
-    if (user_number2 != "yes") and (user_number2 != "no"):
-        print("Invalid answer, please enter 'yes' or 'no'.")
-    elif user_number2 == "no":
-        print("Ok, glad I could help!")
-        sleep(4)
-        break
-    elif user_number2 == "yes":
-        while True:
-            user_number2 = input("Ok! Please enter a desired length: ")
-            try:
-                n2 = int(user_number2)
-                break
-            except ValueError:
-                print("It has to be a number!")            
-
-        while n2 < 0:
-            print("Invalid number!\n")
-            n2 = int(input("Try again, positive value this time: "))
-        print("You got it! Here comes the Fibonacci sequence:")
-        for n in range(n2):
-            print(fibonacci(n))
+    while ask_for_more():
+        n = get_number()
+        print(fibonacci(n))
